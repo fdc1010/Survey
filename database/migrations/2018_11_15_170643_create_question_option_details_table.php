4a16f0adaddd061ece4ceb5be90373fc07f9eabe
franklin caruana <franklincaruana@gmail.com>
2018-11-16 01:50:02 +0800

    updating

--- database/migrations/2018_11_15_170643_create_question_option_details_table.php
+++ database/migrations/2018_11_15_170643_create_question_option_details_table.php
@@ -15,11 +15,13 @@ class CreateQuestionOptionDetailsTable extends Migration
     {
         Schema::create('question_option_details', function (Blueprint $table) {
             $table->increments('id');
+			$table->unsignedInteger('question_id');
 			$table->unsignedInteger('option_id');
 			$table->unsignedInteger('sub_option_id');
 			$table->longText('description')->nullable();
             $table->timestamps();
 			
+			$table->foreign('question_id')->references('id')->on('questions');
 			$table->foreign('option_id')->references('id')->on('question_options');
 			$table->foreign('sub_option_id')->references('id')->on('question_sub_options');
         });
