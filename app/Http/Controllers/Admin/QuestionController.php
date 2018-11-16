96d2435fdfc123e705c03129f7c39f2f3d9648c3
franklin caruana <franklincaruana@gmail.com>
2018-11-16 13:44:07 +0800

    updating

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -21,7 +21,7 @@ class QuestionController extends Controller
 		$result = QuestionDetail::with(['question'=>function($q){
 											$q->select(['id','question','number_answers','priority',
 													'type_id','for_position','with_other_ans',
-													'with_partyselect']);	
+													'with_partyselect'])->groupby('id');	
 										},'option'=>function($o){
 											$o->select(['id','option','priority']);
 										}])->get();
