f1b331ad746fbfb030ad4e774c696f7040c8d679
franklin caruana <franklincaruana@gmail.com>
2018-11-16 12:50:43 +0800

    updating

--- app/Http/Controllers/Admin/QuestionCrudController.php
+++ app/Http/Controllers/Admin/QuestionCrudController.php
@@ -136,7 +136,7 @@ class QuestionCrudController extends CrudController
         // your additional operations after save here
         // use $this->data['entry'] or $this->crud->entry
 		//$qid = $this->crud->entry->id; // <-- SHOULD WORK
-		$options = $this->crud->entry;
+		$options = $this->crud->entry->options;
 		dd($options);
 		foreach($options as $option){
 			$opsval = $option['option'];
