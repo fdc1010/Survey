e3c1c57cc44f76d37934ed13b95fc78a51fc93ff
franklin caruana <franklincaruana@gmail.com>
2018-11-16 08:54:56 +0800

    updating

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -30,7 +30,7 @@ class Question extends Model
 	
 	public function optiondetail()
     {
-        return $this->belongsToMany('App\Models\QuestionDetail','question_id');
+        return $this->hasMany('App\Models\QuestionDetail');
     }
 	public function type()
     {
