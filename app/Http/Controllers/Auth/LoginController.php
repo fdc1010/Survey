d43e058656878aa24719b22226384e92bd662609
franklin caruana <franklincaruana@gmail.com>
2018-11-20 14:28:38 +0800

    updating

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,27 +31,60 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-	public function login(Request $request){
-		info($request);
-	}
+	public function login(Request $request)
+    {
+		if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+			return redirect()->intended('/admin');
+				
+		}
+		else {
+			if(Auth::check()){
+				Auth::logout();
+				$errors = [$this->username() => trans('auth.suspend')];
+				return redirect('/login')
+					->withInput($request->only($this->username(), 'remember'))
+					->withErrors($errors);
+			}
+		}
+			/*else if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'suspend' => 0, 'is_online'=> 1])) {
+				if(Auth::check()){
+					Auth::logout();
+					$errors = [$this->username() => trans('auth.alreadylogin')];
+					return redirect('/login')
+						->withInput($request->only($this->username(), 'remember'))
+						->withErrors($errors);
+				}
+
+			}*/
+			else{
+				$errors = [$this->username() => trans('auth.failed')];
+				return redirect()->back()
+					->withInput($request->only($this->username(), 'remember'))
+					->withErrors($errors);
+			}
+		}
+		
+
+    }
+
+    public function username()
+    {
+        return 'email';
+    }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-	
-	public function logout(Request $request)
-    {
-        $this->guard()->logout();
-
-        $request->session()->invalidate();
-
-        return backpack_url('/login');
     }
 }
