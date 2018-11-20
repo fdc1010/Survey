741106ac2410632ec0e16120dbadb2a55231a473
franklin caruana <franklincaruana@gmail.com>
2018-11-20 14:57:55 +0800

    updating

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,30 @@
 */
 Route::get('getQuestions','QuestionController@getQuestions');
 Route::post('storeAnswers','SurveyAnswerController@storeAnswers');
-Route::get('importexcel', 'VoterController@index')->name('index');
-Route::post('importprecinct', 'VoterController@importprecinct')->name('importprecinct');
-Route::post('importvoters', 'VoterController@importvoters')->name('importvoters');
-Route::post('importbarangays', 'BarangayController@importbarangays')->name('importbarangays');
-Route::post('importsitios', 'SitioController@importsitios')->name('importsitios');
-Route::get('extramiddlename', 'VoterController@extramiddlename')->name('extramiddlename');
+
 //Route::get('media/user/{user}/{collection}', 'VoterController@getMedia');
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::auth();
 Route::group(['prefix' => config('backpack.base.route_prefix', 'admin'), 'middleware' => ['web', 'auth'], 'namespace' => 'Admin'], function () {
     // Backpack\MenuCRUD
 	
 	Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
-    CRUD::resource('menu-item', 'MenuItemCrudController');
-	
+    CRUD::resource('menu-item', 'Admin\MenuItemCrudController');
+	Route::get('importexcel', 'VoterController@index')->name('index');
+	Route::post('importprecinct', 'VoterController@importprecinct')->name('importprecinct');
+	Route::post('importvoters', 'VoterController@importvoters')->name('importvoters');
+	Route::post('importbarangays', 'BarangayController@importbarangays')->name('importbarangays');
+	Route::post('importsitios', 'SitioController@importsitios')->name('importsitios');
+	Route::get('extramiddlename', 'VoterController@extramiddlename')->name('extramiddlename');
+	Route::post('mobilelogin', 'MobileAuthController@mobilelogin');
 });
 /** CATCH-ALL ROUTE for Backpack/PageManager - needs to be at the end of your routes.php file  **/
 Route::get('{page}/{subs?}', ['uses' => 'PageController@index'])
     ->where(['page' => '^((?!admin).)*$', 'subs' => '.*']);
 	
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('mobilelogin', 'MobileAuthController@mobilelogin');
-Route::auth();
-Route::group(['middleware' => ['auth']], function () {
-	Route::auth();
-	Route::get('mobilelogout', 'MobileAuthController@mobilelogout');
-});
+
+
