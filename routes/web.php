60f8dcd4f71d9438c5e61f6e20ce275129a008d7
franklin caruana <franklincaruana@gmail.com>
2018-11-20 18:26:42 +0800

    updating

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 Route::group([
 	 'prefix' => config('backpack.base.route_prefix', 'admin'),
-	 'middleware' => ['web', 'auth','admin'], 
+	 'middleware' => ['web', 'auth'], 
 	 'namespace' => 'Admin'], function () {
     // Backpack\MenuCRUD	
     CRUD::resource('menu-item', 'MenuItemCrudController');
