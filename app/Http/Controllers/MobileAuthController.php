9c09756eb9cd8a3109e912cc4912af3bb9dfd02c
franklin caruana <franklincaruana@gmail.com>
2018-11-20 13:33:25 +0800

    updating

--- app/Http/Controllers/MobileAuthController.php
+++ app/Http/Controllers/MobileAuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
