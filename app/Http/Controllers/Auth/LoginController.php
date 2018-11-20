f3627d6a481e87680c434d2af7c550f0afb0f2cc
franklin caruana <franklincaruana@gmail.com>
2018-11-20 14:59:43 +0800

    updating

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Admin\Controller;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
