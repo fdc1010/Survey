353ee81dd25adbd57f642ca61e5bbf9f904ca013
franklin caruana <franklincaruana@gmail.com>
2018-11-20 20:01:16 +0800

    updating

--- app/Http/Controllers/Mobile/MobileController.php
+++ app/Http/Controllers/Mobile/MobileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Mobile;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Response;
