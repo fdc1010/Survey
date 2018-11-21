aeb0a9f44bda0321658369d4facbf3b8e3450d1e
franklin caruana <franklincaruana@gmail.com>
2018-11-21 17:34:23 +0800

    updating

--- resources/views/vendor/backpack/crud/fields/tableadv.blade.php
+++ resources/views/vendor/backpack/crud/fields/tableadv.blade.php
@@ -113,7 +113,7 @@
                                 
                                         @if (isset($field['columns']['model']))
                                             @foreach ($field['columns']['model']::with($field['columns']['entity'])->get() as $parent_connected_entity_entry)
-											     <optgroup label="{{ $parent_connected_entity_entry->{$field['columns']['attribute']} }}">
+											     <optgroup style="font-size:21px !important;" class="" label="{{ $parent_connected_entity_entry->{$field['columns']['attribute']} }}">
 													@foreach ($parent_connected_entity_entry->{$field['columns']['entity']} as $connected_entity_entry)
                                                           @if($current_value == $connected_entity_entry->getKey())
                                                               <option value="{{ $connected_entity_entry->getKey() }}" selected>{{ $connected_entity_entry->{$field['columns']['attribute']} }}</option>
