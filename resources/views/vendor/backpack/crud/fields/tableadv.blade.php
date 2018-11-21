fbfdfab08d4c52b290f4499ad4c7c951c0cc17f1
franklin caruana <franklincaruana@gmail.com>
2018-11-21 17:25:02 +0800

    updating

--- resources/views/vendor/backpack/crud/fields/tableadv.blade.php
+++ resources/views/vendor/backpack/crud/fields/tableadv.blade.php
@@ -104,7 +104,7 @@
                                     <?php $entity_model = $crud->getRelationModel($field['columns']['entity'],  - 1); ?>
                                     <select ng-model="item.{{ $prop }}"
                                     	style="width: 250px;"
-                                        @include('crud::inc.field_attributes', ['default_class' =>  'form-control select2_field'])
+                                        @include('crud::inc.field_attributes', ['default_class' =>  'form-control select2'])
                                         >
                                 
                                         @if ($entity_model::isColumnNullable($field['columns']['name']))
@@ -113,7 +113,7 @@
                                 
                                         @if (isset($field['columns']['model']))
                                             @foreach ($field['columns']['model']::with($field['columns']['entity'])->get() as $parent_connected_entity_entry)
-											     <optgroup label="{{ $parent_connected_entity_entry->{$field['columns']['attribute']} }}" style="font-size:36px !important;">
+											     <optgroup label="{{ $parent_connected_entity_entry->{$field['columns']['attribute']} }}">
 													@foreach ($parent_connected_entity_entry->{$field['columns']['entity']} as $connected_entity_entry)
                                                           @if($current_value == $connected_entity_entry->getKey())
                                                               <option value="{{ $connected_entity_entry->getKey() }}" selected>{{ $connected_entity_entry->{$field['columns']['attribute']} }}</option>
