35a2ffe8bd7aa58c772a7d23127b31aeee908f8f
franklin caruana <franklincaruana@gmail.com>
2018-11-22 02:59:44 +0800

    updating

--- app/Models/Precinct.php
+++ app/Models/Precinct.php
@@ -29,9 +29,8 @@ class Precinct extends Model
     }
 	public function getPrecintInfoAttribute()
     {
-		$brgy = Barangay::find($this->barangay_id);
-		info($brgy);
-        return $this->attributes['precinct_number'] . ' (' . $brgy->name . ')';
+		
+        return $this->attributes['precinct_number'] . ' (' . $this->attributes['barangay.name'] . ')';
     }
     /*
     |--------------------------------------------------------------------------
