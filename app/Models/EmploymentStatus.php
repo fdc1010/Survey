520ce2630b4ed82426134b077a626b6540a0c1e8
franklin caruana <franklincaruana@gmail.com>
2018-11-22 13:55:22 +0800

    update

--- app/Models/EmploymentStatus.php
+++ app/Models/EmploymentStatus.php
@@ -19,7 +19,7 @@ class EmploymentStatus extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     // protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = ['name','description'];
     // protected $hidden = [];
     // protected $dates = [];
 
--- app/Models/OccupancyStatus.php
+++ app/Models/OccupancyStatus.php
@@ -19,7 +19,7 @@ class OccupancyStatus extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     // protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = ['name','description'];
     // protected $hidden = [];
     // protected $dates = [];
 
