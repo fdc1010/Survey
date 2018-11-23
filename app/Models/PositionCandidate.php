7e28944339629004ff8f7c900c3970bfec06c470
franklin caruana <franklincaruana@gmail.com>
2018-11-23 22:22:21 +0800

    updating

--- app/Models/PositionCandidate.php
+++ app/Models/PositionCandidate.php
@@ -26,9 +26,12 @@ class PositionCandidate extends Model
         'options' => 'array'
     ];
 	
-	/*public function optionsposition(){
+	public function positionoptions(){
 		return $this->hasMany('App\Models\OptionPosition','position_id');
-	}*/
+	}
+	public function optionsposition(){
+		return $this->hasMany('App\Models\OptionPosition','option_id');
+	}
 	public function optionsposition(){
 		return $this->belongsToMany('App\Models\PositionCandidate','option_positions','option_id','position_id');
 	}
