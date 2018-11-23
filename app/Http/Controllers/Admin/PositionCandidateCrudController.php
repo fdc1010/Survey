88c866a757f9fcd42bf95f2d1be3363424f45f11
franklin caruana <franklincaruana@gmail.com>
2018-11-23 18:42:14 +0800

    update

--- app/Http/Controllers/Admin/PositionCandidateCrudController.php
+++ app/Http/Controllers/Admin/PositionCandidateCrudController.php
@@ -85,14 +85,14 @@ class PositionCandidateCrudController extends CrudController
 		$position = $this->crud->entry->id; // <-- SHOULD WORK
 		$opdetail = OptionPosition::where('position_id',$position)->delete();		
 		$options = $this->crud->entry->option_selection;
-		dd($this->crud->entry);		
+				
 		foreach($options as $option){
 			$optionposition = OptionPosition::create([
 				'position_id' => $position,
 				'option_id' => $option
 			]);			
 		}
-		
+		dd($this->crud->entry);
         return $redirect_location;
     }
 }
