b3a0a43da51d43db129162e6797a3331dc185655
franklin caruana <franklincaruana@gmail.com>
2018-11-23 12:54:01 +0800

    update

--- app/Http/Controllers/Admin/QuestionOptionCrudController.php
+++ app/Http/Controllers/Admin/QuestionOptionCrudController.php
@@ -35,7 +35,7 @@ class QuestionOptionCrudController extends CrudController
         // TODO: remove setFromDb() and manually define Fields and Columns
         $this->crud->setFromDb();		
 		$this->crud->addColumn([
-            'name' => 'positions',			
+            'name' => 'position',			
             'label' => 'Positions Tagged',
             'type' => 'model_function',
 			'function_name' => 'getPositions',
