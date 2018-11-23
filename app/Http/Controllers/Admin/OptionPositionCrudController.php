90531363d5e8f2ae5a7d490e401ea5246677b555
franklin caruana <franklincaruana@gmail.com>
2018-11-23 13:07:01 +0800

    update

--- app/Http/Controllers/Admin/OptionPositionCrudController.php
+++ app/Http/Controllers/Admin/OptionPositionCrudController.php
@@ -39,7 +39,7 @@ class OptionPositionCrudController extends CrudController
 		
 		$this->crud->addColumn([
             'name' => 'position_id',
-            'type' => 'select',
+            'type' => 'checklist',
             'label' => 'Position',
 			'entity' => 'position', // the relationship name in your Model
 			'attribute' => 'name', // attribute on Article that is shown to admin
