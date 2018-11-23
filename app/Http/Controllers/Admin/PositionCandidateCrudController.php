8f0ac8dfa37f03e3c91a7274f6afe5d0b496c9c6
franklin caruana <franklincaruana@gmail.com>
2018-11-23 18:45:03 +0800

    update

--- app/Http/Controllers/Admin/PositionCandidateCrudController.php
+++ app/Http/Controllers/Admin/PositionCandidateCrudController.php
@@ -82,10 +82,11 @@ class PositionCandidateCrudController extends CrudController
         $redirect_location = parent::updateCrud($request);
         // your additional operations after save here
         // use $this->data['entry'] or $this->crud->entry
+		dd($this->crud->entry);
 		$position = $this->crud->entry->id; // <-- SHOULD WORK
 		$opdetail = OptionPosition::where('position_id',$position)->delete();		
 		$options = $this->crud->entry->option_selection;
-		dd($this->crud->entry);
+		
 		foreach($options as $option){
 			$optionposition = OptionPosition::create([
 				'position_id' => $position,
