06a4045cd338656282c739706d4d483ac3c85f52
franklin caruana <franklincaruana@gmail.com>
2018-11-23 18:04:29 +0800

    update

--- app/Models/PositionCandidate.php
+++ app/Models/PositionCandidate.php
@@ -25,7 +25,7 @@ class PositionCandidate extends Model
 	protected $appends = ['option_selections'];
 	
 	public function optionsposition(){
-		return $this->belongsToMany('App\Models\QuestionOption','option_positions','position_id','option_id');
+		return $this->hasMany('App\Models\QuestionOption','position_id');
 	}
 	public function getOptionSelectionsAttribute(){
 		/*$options = OptionPosition::with('options')->where('position_id',$this->id)->get();
