c5f83937c9c3106e7bb72d3560c3dc79649b2878
franklin caruana <franklincaruana@gmail.com>
2018-11-23 17:54:10 +0800

    update

--- app/Http/Controllers/Admin/PositionCandidateCrudController.php
+++ app/Http/Controllers/Admin/PositionCandidateCrudController.php
@@ -19,7 +19,7 @@ class PositionCandidateCrudController extends CrudController
     public function setup()
     {
 		$pos = PositionCandidate::first();
-		dd($pos);
+		dd($pos->options);
         /*
         |--------------------------------------------------------------------------
         | CrudPanel Basic Information
