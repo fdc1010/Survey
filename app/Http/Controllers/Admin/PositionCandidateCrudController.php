c73b837510cf60cd51424949fc26025ddce64161
franklin caruana <franklincaruana@gmail.com>
2018-11-26 00:44:35 +0800

    updating

--- app/Http/Controllers/Admin/PositionCandidateCrudController.php
+++ app/Http/Controllers/Admin/PositionCandidateCrudController.php
@@ -48,7 +48,7 @@ class PositionCandidateCrudController extends CrudController
             'name' => 'options',
             'type' => 'checklist',
             'label' => 'Options',
-			'entity' => 'position', // the relationship name in your Model
+			'entity' => 'positionoptions', // the relationship name in your Model
 			'attribute' => 'option', // attribute on Article that is shown to admin
 			'model' => "App\Models\QuestionOption",
 			/*'fake' => true,
