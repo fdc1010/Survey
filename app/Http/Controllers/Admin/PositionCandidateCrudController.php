31379a0c026d06b336c9c9ae3a73a19797bc194b
franklin caruana <franklincaruana@gmail.com>
2018-11-25 23:16:54 +0800

    updating

--- app/Http/Controllers/Admin/PositionCandidateCrudController.php
+++ app/Http/Controllers/Admin/PositionCandidateCrudController.php
@@ -36,8 +36,8 @@ class PositionCandidateCrudController extends CrudController
 
         // TODO: remove setFromDb() and manually define Fields and Columns
         $this->crud->setFromDb();
-		$this->crud->removeColumn(['options']);
-		$this->crud->removeField(['options']);
+		//$this->crud->removeColumn(['options']);
+		//$this->crud->removeField(['options']);
 		$this->crud->addColumn([
             'name' => 'extras',			
             'label' => 'Tagged Options for Qualities',
