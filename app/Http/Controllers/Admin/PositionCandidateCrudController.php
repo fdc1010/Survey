c82c228df861e5d7775598dd04535eba0c6dc09a
franklin caruana <franklincaruana@gmail.com>
2018-11-25 22:18:45 +0800

    updating

--- app/Http/Controllers/Admin/PositionCandidateCrudController.php
+++ app/Http/Controllers/Admin/PositionCandidateCrudController.php
@@ -48,9 +48,9 @@ class PositionCandidateCrudController extends CrudController
             'name' => 'options',
             'type' => 'checklist',
             'label' => 'Qualities Tagged Options',
-			'entity' => 'optionselections', // the relationship name in your Model
 			'attribute' => 'option', // attribute on Article that is shown to admin
-			'model' => "App\Models\QuestionOption"
+			'model' => "App\Models\QuestionOption",
+			'fake'=>true
 	    ]);
         // add asterisk for fields that are required in PositionCandidateRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
