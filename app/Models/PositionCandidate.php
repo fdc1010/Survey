72f9dc0ab9011fd1a95c6dee0825d9a0b147307e
franklin caruana <franklincaruana@gmail.com>
2018-11-26 00:26:08 +0800

    updating

--- app/Models/PositionCandidate.php
+++ app/Models/PositionCandidate.php
@@ -38,7 +38,7 @@ class PositionCandidate extends Model
 		return $this->belongsTo('App\Models\OptionPosition');
 	}
 	public function options(){
-		return $this->belongsTo('App\Models\OptionPosition');
+		return $this->belongsTo('App\Models\QuestionOption');
 	}
 	public function optionselections(){
 		return $this->belongsTo('App\Models\QuestionOption','option_id');
