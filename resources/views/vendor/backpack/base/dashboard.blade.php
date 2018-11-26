8a2685d495697a6d8f88a886a13851281791a752
franklin caruana <franklincaruana@gmail.com>
2018-11-26 15:05:19 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -423,12 +423,12 @@
 			@foreach($problems as $problem)
 				'{{ $problem->option->option }}',
 			@endforeach
-			],			
-			@foreach($problems as $problem)				
-			['{{ $problem->option->option }}',
-				{{ $tallybrgy[$barangay->id][$problem->option_id] }},
 			],
-			@endforeach				
+			['Tally',
+			@foreach($problems as $problem)				
+				{{ $tallybrgy[$barangay->id][$problem->option_id] }},
+			@endforeach
+				],
           ],
           type: 'bar',
           onclick: function (d, element) { console.log("onclick", d, element); },
