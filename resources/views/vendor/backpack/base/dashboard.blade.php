5f5491cee31eb66afc65eaffb222f4687958b6be
franklin caruana <franklincaruana@gmail.com>
2018-11-26 19:30:02 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -57,7 +57,7 @@
                                 </div>
                             </form>
                             @if(!empty($vstatus))
-                            	{!! $vstatus !!}
+                            	{{ var_dump($vstatus) }}
                             @endif
                          </div>
                     </div>
