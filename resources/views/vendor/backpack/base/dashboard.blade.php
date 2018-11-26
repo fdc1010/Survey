ed1326fee9b3a5e50c3b0df268cb780fee12c5dd
franklin caruana <franklincaruana@gmail.com>
2018-11-26 18:54:50 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -43,7 +43,7 @@
                                             </select>
                                     </div>
                                     <div class="col-md-4">
-                                        <select id="selvoterstatuses" name="selvoterstatuses[]" class="select2_multiple" multiple="multiple">                                  
+                                        <select id="selvoterstatuses" name="selvoterstatuses[]" multiple="multiple">                                  
                                             @foreach($voterstatuses as $voterstatus)
                                                 <option value="{{ $voterstatus->id }}">{{ $voterstatus->name }}</option>
                                             @endforeach
