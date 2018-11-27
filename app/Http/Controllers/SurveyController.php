d865ce6416d702075c71b53cf07cd45b47e94014
franklin caruana <franklincaruana@gmail.com>
2018-11-27 13:52:37 +0800

    updating

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Survey;
+use App\Models\Voter;
 use Illuminate\Http\Request;
 
 class SurveyController extends Controller
@@ -69,7 +70,44 @@ class SurveyController extends Controller
      */
     public function update(Request $request, Survey $survey)
     {
-        //
+		$voter = Voter::find($voter_id);
+        $voter->fill($request->all());
+		
+		if($request->hasFile('profilepic')){
+			
+			$path = config('app.root') . '/public/profilepic/';
+			$photo=$path.basename($voter->profilepic);
+			
+			File::delete($photo);
+			
+            $md5profName = md5_file($request->file('profilepic')->getRealPath());
+            $guessExtensionprof = $request->file('profilepic')->guessExtension();
+
+            $srvroot = $_SERVER['DOCUMENT_ROOT'];
+            $pathimage =  $srvroot . '/profilepic/';
+            $path = url('/profilepic/');
+            if (!File::exists($path)) {
+                File::makeDirectory($path,0777);
+            }
+
+            $width = 160;
+            $height = 160;
+            $image = Image::make($request->file('profilepic')->getRealPath());
+            $image->width() > $image->height() ? $width=null : $height=null;
+            $image->resize($width, $height, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+
+            $image->save($pathimage.$md5profName.'.'.$guessExtensionprof);
+
+            $filename = $md5profName.'.'.$guessExtensionprof;
+            $voter->profilepic =  config('app.url') . '/profilepic/' . $filename;
+
+        }
+
+
+        $voter->save();
     }
 
     /**
