ddb2b23b6fbdecf044a8302cacf0eb29bcdcaa71
franklin caruana <franklincaruana@gmail.com>
2018-11-27 19:33:52 +0800

    updating

--- app/Http/Controllers/Admin/StatusDetailCrudController.php
+++ app/Http/Controllers/Admin/StatusDetailCrudController.php
@@ -34,6 +34,8 @@ class StatusDetailCrudController extends CrudController
 
         // TODO: remove setFromDb() and manually define Fields and Columns
         $this->crud->setFromDb();
+		$this->crud->removeColumn(['status_id','voter_id']);
+		$this->crud->removeField(['status_id','voter_id']);
 		$this->crud->addColumn([
             'name' => 'voter_id',
             'type' => 'select',
@@ -41,6 +43,14 @@ class StatusDetailCrudController extends CrudController
 			'entity' => 'voter', // the relationship name in your Model
 			'attribute' => 'full_name', // attribute on Article that is shown to admin
 			'model' => "App\Models\Voter"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'status_id',
+            'type' => 'select',
+            'label' => 'Status',
+			'entity' => 'status', // the relationship name in your Model
+			'attribute' => 'status_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\VoterStatus"
 	    ]);
 		$this->crud->addField([
 			'label' => "Status",
@@ -51,6 +61,14 @@ class StatusDetailCrudController extends CrudController
 			'model' => "App\Models\VoterStatus", // on create&update, do you need to add/delete pivot table entries?
 			//'attribute2' => 'name', // attribute on Article that is shown to admin
 			//'entity2' => "barangay"
+		]);
+		$this->crud->addField([
+			'name' => 'voter_id',
+            'type' => 'select',
+            'label' => 'Voter',
+			'entity' => 'voter', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Voter"
 		]);
         // add asterisk for fields that are required in StatusDetailRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
