da460ba1472d4f7eca9f1dac1999e56544a2ed89
franklin caruana <franklincaruana@gmail.com>
2018-11-28 15:26:57 +0800

    updating

--- app/Http/Controllers/Admin/TallyOtherVoteCrudController.php
+++ app/Http/Controllers/Admin/TallyOtherVoteCrudController.php
@@ -24,7 +24,7 @@ class TallyOtherVoteCrudController extends CrudController
         */
         $this->crud->setModel('App\Models\TallyOtherVote');
         $this->crud->setRoute(config('backpack.base.route_prefix') . '/tallyothervote');
-        $this->crud->setEntityNameStrings('tallyothervote', 'tally_other_votes');
+        $this->crud->setEntityNameStrings('tally other vote', 'Tally Other Votes');
 
         /*
         |--------------------------------------------------------------------------
@@ -34,7 +34,58 @@ class TallyOtherVoteCrudController extends CrudController
 
         // TODO: remove setFromDb() and manually define Fields and Columns
         $this->crud->setFromDb();
-
+		$this->crud->removeColumns(['voter_id','candidate_id','survey_detail_id']);
+		$this->crud->removeFields(['voter_id','candidate_id','survey_detail_id']);
+		$this->crud->orderBy('candidate_id');
+		
+		$this->crud->addColumn([
+            'name' => 'voter_id',
+            'type' => 'select',
+            'label' => 'Voter',
+			'entity' => 'voter', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Voter"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'option_id',
+            'type' => 'select',
+            'label' => 'Option',
+			'entity' => 'option', // the relationship name in your Model
+			'attribute' => 'option', // attribute on Article that is shown to admin
+			'model' => "App\Models\QuestionOption"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'survey_detail_id',
+            'type' => 'select',
+            'label' => 'Survey',
+			'entity' => 'surveydetail', // the relationship name in your Model
+			'attribute' => 'subject', // attribute on Article that is shown to admin
+			'model' => "App\Models\SurveyDetail"
+	    ]);
+		$this->crud->addField([
+            'name' => 'voter_id',
+            'type' => 'select2',
+            'label' => 'Voter',
+			'entity' => 'voter', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Voter"
+	    ]);
+		$this->crud->addField([
+            'name' => 'option_id',
+            'type' => 'select2',
+            'label' => 'Option',
+			'entity' => 'option', // the relationship name in your Model
+			'attribute' => 'option', // attribute on Article that is shown to admin
+			'model' => "App\Models\QuestionOption"
+	    ]);
+		$this->crud->addField([
+            'name' => 'survey_detail_id',
+            'type' => 'select2',
+            'label' => 'Survey',
+			'entity' => 'surveydetail', // the relationship name in your Model
+			'attribute' => 'subject', // attribute on Article that is shown to admin
+			'model' => "App\Models\SurveyDetail"
+	    ]);
         // add asterisk for fields that are required in TallyOtherVoteRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
