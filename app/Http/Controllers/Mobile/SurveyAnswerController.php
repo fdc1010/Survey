1eae6bdc991ab83d737226f456e95ca43a60baff
franklin caruana <franklincaruana@gmail.com>
2018-11-30 19:53:10 +0800

    updating

--- app/Http/Controllers/Mobile/SurveyAnswerController.php
+++ app/Http/Controllers/Mobile/SurveyAnswerController.php
@@ -34,7 +34,7 @@ class SurveyAnswerController extends Controller
 		$voterid = $request->voter_id;
 		$surveydetailid = $request->survey_detail_id;
 		
-		$voterdetails = json_decode($request->voter_detail);
+		$voterdetails = json_encode($request->voter_detail);
 		
 		info($voterdetails);
 		/*Voter::where('id',$voterid)
