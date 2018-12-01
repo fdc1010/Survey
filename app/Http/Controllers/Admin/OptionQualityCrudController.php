c17ddbcc91f75b7bee84d4e17ef4da2920800209
franklin caruana <franklincaruana@gmail.com>
2018-12-02 02:07:55 +0800

    add printing

--- app/Http/Controllers/Admin/OptionQualityCrudController.php
+++ app/Http/Controllers/Admin/OptionQualityCrudController.php
@@ -33,7 +33,7 @@ class OptionQualityCrudController extends CrudController
         */
 
         // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
+        //$this->crud->setFromDb();
 		//$this->crud->removeColumns(['option_id','position_id','extras','extras_2']);
 		$this->crud->removeFields(['option_id','description']);
 		$this->crud->addColumn([
