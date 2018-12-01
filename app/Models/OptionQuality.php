373afd5caa33ae93c1425778f8aeec8f72c60581
franklin caruana <franklincaruana@gmail.com>
2018-12-02 03:25:20 +0800

    add printing

--- app/Models/OptionQuality.php
+++ app/Models/OptionQuality.php
@@ -32,17 +32,18 @@ class OptionQuality extends Model
     {
         return $this->belongsTo('App\Models\QuestionOption','option_id');
     }
-	public function position()
+	public function positions(){
+		return $this->hasMany('App\Models\OptionPosition');
+    }
+	/*public function position()
     {
         return $this->belongsTo('App\Models\PositionCandidate');
     }
-	/*public function options()
+	public function options()
     {
         return $this->hasMany('App\Models\OptionPosition','option_id');
     }
-	public function positions(){
-		return $this->hasMany('App\Models\OptionPosition','position_id');
-    }
+	
 	public function options_and_positions()
     {
         return $this->belongsToMany('App\Models\QuestionOption','option_positions');
