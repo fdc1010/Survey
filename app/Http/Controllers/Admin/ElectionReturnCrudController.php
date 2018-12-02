dd793d4cf78ee9748b8addd0379d89acb42855ec
franklin caruana <franklincaruana@gmail.com>
2018-12-02 21:01:56 +0800

    add printing

--- app/Http/Controllers/Admin/ElectionReturnCrudController.php
+++ app/Http/Controllers/Admin/ElectionReturnCrudController.php
@@ -33,8 +33,69 @@ class ElectionReturnCrudController extends CrudController
         */
 
         // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
-
+        //$this->crud->setFromDb();
+		$this->crud->addColumn([
+            'name' => 'precinct_id',			
+            'label' => 'Precinct',
+            'type' => 'model_function',
+			'function_name' => 'getPrecinct'
+	    ])->makeFirstColumn();
+		$this->crud->addColumn([
+            'name' => 'election_id',
+            'type' => 'select',
+            'label' => 'Election',
+			'entity' => 'election', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Election"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'candidate_id',
+            'type' => 'select',
+            'label' => 'Candidate',
+			'entity' => 'candidate', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\PositionCandidate"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'voter_id',
+            'type' => 'select',
+            'label' => 'Voter',
+			'entity' => 'voter', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Voter"
+	    ]);
+		$this->crud->addField([
+            'name' => 'election_id',
+            'type' => 'select2',
+            'label' => 'Election',
+			'entity' => 'election', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Election"
+	    ]);
+		$this->crud->addField([
+            'name' => 'precinct_id',
+            'type' => 'select2',
+            'label' => 'Precinct',
+			'entity' => 'precinct', // the relationship name in your Model
+			'attribute' => 'precinct_info', // attribute on Article that is shown to admin
+			'model' => "App\Models\Precinct"
+	    ]);
+		$this->crud->addField([
+            'name' => 'candidate_id',
+            'type' => 'select2',
+            'label' => 'Candidate',
+			'entity' => 'candidate', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\PositionCandidate"
+	    ]);
+		$this->crud->addField([
+            'name' => 'voter_id',
+            'type' => 'select2',
+            'label' => 'Voter',
+			'entity' => 'voter', // the relationship name in your Model
+			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Voter"
+	    ]);
         // add asterisk for fields that are required in ElectionReturnRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
--- app/Models/ElectionReturn.php
+++ app/Models/ElectionReturn.php
@@ -19,10 +19,29 @@ class ElectionReturn extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     // protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = ['election_id','candidate_id','voter_id','precinct_id','subject','description'];
     // protected $hidden = [];
     // protected $dates = [];
-
+	public function election()
+    {
+        return $this->belongsTo('App\Models\Election','election_id');
+    }
+	public function candidate()
+    {
+        return $this->belongsTo('App\Models\Candidate','candidate_id');
+    }
+	public function voter()
+    {
+        return $this->belongsTo('App\Models\Candidate','voter_id');
+    }
+	public function precinct()
+    {
+        return $this->belongsTo('App\Models\Candidate','precinct_id');
+    }
+	public function getPrecinct(){
+		$precinct = Precinct::find($this->precinct_id)->with('barangay');
+		return $precinct->precinct_number . " (" . $precinct->barangay->name . ")";
+	}
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
