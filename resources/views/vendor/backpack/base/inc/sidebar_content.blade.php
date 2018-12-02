b185c144bdf90a45b87a37639866a90dd661e288
franklin caruana <franklincaruana@gmail.com>
2018-12-02 23:27:36 +0800

    add election returns

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -57,7 +57,14 @@
             <li><a href='{{ backpack_url('optionquality') }}'><i class='fa fa-check-square-o'></i> <span>Option Tag Qualities</span></a></li>
   		</ul>
     </li>
-    <li><a href='{{ backpack_url('electionreturn') }}'><i class='fa fa-bar-chart'></i> <span>Election Returns</span></a></li>
+    <li class="treeview">
+      	<a href="#"><i class="fa fa-tag"></i> <span>Election</span> <i class="fa fa-angle-left pull-right"></i></a>
+      	<ul class="treeview-menu">
+    		<li><a href='{{ backpack_url('election') }}'><i class='fa fa-check-square-o'></i> <span>Details</span></a></li>
+            <li><a href='{{ backpack_url('electionreturn') }}'><i class='fa fa-bar-chart'></i> <span>Election Returns</span></a></li>
+  		</ul>
+    </li>
+    
   </ul>
 </li>
 <li class="treeview">
