3fb383f5e68dac905c987c1fa398aef378310e27
franklin caruana <franklincaruana@gmail.com>
2018-12-03 00:35:00 +0800

    add election returns

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -246,12 +246,13 @@
                                     @endforeach
                                     </select>
                                 </div>
-                                <div class="col-md-2"> 
+                                <div class="col-md-1"> 
                                     <a href="#" id="printpreview"><span class="fa fa-file-pdf-o"></span> Print Preview</a>
                                 </div>
-                                <div class="col-md-2"> 
+                                <div class="col-md-1"> 
                                 	<a href="#" id="btn_printdetails"><span class="fa fa-plus" id="spanprintdetails"> </span></a>
                                 </div>
+                                <div class="col-md-2">Compare:</div>
                                 <div class="col-md-2"> 
                                     <select name="selsurveycompare" id="selsurveycompare">
                                     @foreach($surveydetails as $surveydetail)	
