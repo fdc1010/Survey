ec2c47065de698a73ca257cbe5e0a1faa5e1b3d4
franklin caruana <franklincaruana@gmail.com>
2018-12-03 14:34:37 +0800

    updating dashboard

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -632,7 +632,7 @@
             </div>
         </div>
         </form>
-    	<div class="row">    	
+    	    	
     	<div class="col-md-6">
             <div class="box box-default">
                 <div class="box-header with-border">
