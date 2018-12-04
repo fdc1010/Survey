9d1c1ef0855fb2129ffcc23ba39a7c12a4830df9
franklin caruana <franklincaruana@gmail.com>
2018-12-04 11:42:50 +0800

    updates: adding surveyor survey count function

--- app/Http/Requests/SurveyorAssignmentRequest.php
+++ app/Http/Requests/SurveyorAssignmentRequest.php
@@ -27,7 +27,7 @@ class SurveyorAssignmentRequest extends FormRequest
     {
         return [
             // 'name' => 'required|min:5|max:255'
-			'quota' => 'required|integer|min:5',
+			'quota' => 'required|integer',
 			'user_id' => 'required'
         ];
     }
