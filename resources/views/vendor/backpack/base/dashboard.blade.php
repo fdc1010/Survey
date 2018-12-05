04ff1c9aaf216be738b096d819cd0c6be2a1f4bc
franklin caruana <franklincaruana@gmail.com>
2018-12-05 18:21:52 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -189,8 +189,8 @@
         $temppositions = App\Models\PositionCandidate::with(['candidates'=>function($q){
         														$q->with(['tally'=>function($qt){
                                                                 	$qt->select(['candidate_id',DB::raw('count(tally) as tally_count')]);
-                                                                });
-        													})->where('id',$surveypos)->get();        
+                                                                }]);
+        													}])->where('id',$surveypos)->get();        
         if(!empty($rdata['position']) && empty($rdata['selcandidate'])){
             $temppositions = App\Models\PositionCandidate::with('candidates')->whereIn('id',$rdata['position'])->get();
 
