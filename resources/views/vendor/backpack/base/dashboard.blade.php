ca05ec4afb9581924c8a48d2bb9a90e0b4e94d94
franklin caruana <franklincaruana@gmail.com>
2018-12-05 18:39:16 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -247,7 +247,7 @@
                                                             ->get();
             }
         }        
-        $positions = $temppositions->candidates->tally()->groupBy('candidate_id')->sortByDesc('tally_count');
+        $positions = $temppositions->candidates()->tally()->groupBy('candidate_id')->sortByDesc('tally_count');
         
         $tally = array();  
         $tallyg = array(); 
