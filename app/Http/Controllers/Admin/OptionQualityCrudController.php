e6391ea140194a5b9d68bc496307dd13c86741ae
franklin caruana <franklincaruana@gmail.com>
2018-12-06 16:23:53 +0800

    customizing crud fields for options tagging

--- app/Http/Controllers/Admin/OptionQualityCrudController.php
+++ app/Http/Controllers/Admin/OptionQualityCrudController.php
@@ -62,7 +62,7 @@ class OptionQualityCrudController extends CrudController
 		$this->crud->addField([
 			'label' => "Positions",
 			'type' => 'checklistchkall',
-			'name' => 'positions', 
+			'name' => 'position_id', 
 			'entity' => 'positionsoptions',
 			'attribute' => 'name', 
 			'model' => "App\Models\PositionCandidate"
