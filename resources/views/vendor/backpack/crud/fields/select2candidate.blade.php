cce27f87dbfe560acc88659048a50c23d20f8dcf
franklin caruana <franklincaruana@gmail.com>
2018-12-06 14:41:47 +0800

    updated: Editing Candidate Model and CandidateCrudController

--- resources/views/vendor/backpack/crud/fields/select2candidate.blade.php
+++ resources/views/vendor/backpack/crud/fields/select2candidate.blade.php
@@ -20,7 +20,7 @@
         @endif
 
         @if (isset($field['model']))
-            @foreach ($field['model']::doesntHave('$field['entity2']') as $connected_entity_entry)
+            @foreach ($field['model']::doesntHave({$field['entity2']}) as $connected_entity_entry)
                 @if($current_value == $connected_entity_entry->getKey())
                     <option value="{{ $connected_entity_entry->getKey() }}" selected>{{ $connected_entity_entry->{$field['attribute']} }}</option>
                 @else
