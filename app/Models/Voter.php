04d2b538bde81c275115f467256dcc9c7d054403
franklin caruana <franklincaruana@gmail.com>
2018-12-06 14:03:44 +0800

    updated: Adding imei field to users migration

--- app/Models/Voter.php
+++ app/Models/Voter.php
@@ -65,10 +65,7 @@ class Voter extends Model
 	{
 		return $this->hasOne('App\Models\Candidate','voter_id');
 	}
-	public function untaggedcandidate();
-	{
-		return $this->hasNot(Candidate::class,'voter_id');
-	}
+	
 	public function getStatusName(){		
 		$voterstatus = StatusDetail::with('status')->where('voter_id',$this->id)->get();
 		$result = "<ul>";
