6cb15ad7d8e7257be86bcba8f2134170ef8bbd50
franklin caruana <franklincaruana@gmail.com>
2018-12-06 15:26:01 +0800

    adding migration for question_options for_candidate tagging

--- resources/views/vendor/backpack/crud/fields/select2criteria.blade.php
+++ resources/views/vendor/backpack/crud/fields/select2criteria.blade.php
@@ -20,7 +20,7 @@
         @endif
 
         @if (isset($field['model']))
-            @foreach ($field['model']::$field['criteria']->get() as $connected_entity_entry)
+            @foreach ($field['model']::{$field['criteria']}->get() as $connected_entity_entry)
                 @if($current_value == $connected_entity_entry->getKey())
                     <option value="{{ $connected_entity_entry->getKey() }}" selected>{{ $connected_entity_entry->{$field['attribute']} }}</option>
                 @else
