949fdd70719e4abe3135368d13044f5490eda99c
franklin caruana <franklincaruana@gmail.com>
2018-12-06 16:16:20 +0800

    customizing crud fields for options tagging

--- app/Models/OptionQuality.php
+++ app/Models/OptionQuality.php
@@ -30,11 +30,11 @@ class OptionQuality extends Model
 	
 	public function options()
     {
-        return $this->belongsTo('App\Models\QuestionOption','option_id');
+        return $this->hasOne('App\Models\QuestionOption','option_id');
     }
 	public function positions()
     {
-        return $this->belongsTo('App\Models\PositionCandidate','position_id');
+        return $this->hasOne('App\Models\PositionCandidate','position_id');
     }
 	/*public function positions(){
 		return $this->belongsToMany('App\Models\PositionCandidate','option_positions','position_id','position_id');
