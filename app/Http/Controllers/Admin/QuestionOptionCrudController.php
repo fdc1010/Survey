2b704cc55500e12fb434eca0eceba9bc05903fe8
franklin caruana <franklincaruana@gmail.com>
2018-12-08 17:05:24 +0800

    updating

--- app/Http/Controllers/Admin/QuestionOptionCrudController.php
+++ app/Http/Controllers/Admin/QuestionOptionCrudController.php
@@ -102,7 +102,7 @@ class QuestionOptionCrudController extends CrudController
 			'type' => 'checkboxtoggle',
 			'toggle_field' => 'extras'
 	    ]);		
-		dd($id);
+		dd($this->crud->getEntries());
 		$this->crud->addField([
 			'label' => "Positions",
 			'type' => 'checklistchkall3',
