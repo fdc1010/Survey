4bba9f24e3dacd0f6d3aa8780177b67a9f42ef5f
franklin caruana <franklincaruana@gmail.com>
2018-12-08 15:08:19 +0800

    updating

--- resources/views/vendor/backpack/crud/fields/checklistchkall.blade.php
+++ resources/views/vendor/backpack/crud/fields/checklistchkall.blade.php
@@ -12,20 +12,9 @@
     </div>
     <div class="row">
         @foreach ($field['model']::all() as $connected_entity_entry)
-        	{!! $connected_entity_entry->getKey() !!}
-            <div class="col-sm-4">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"
-                      name="{{ $field['name'] }}[]"
-                      value="{{ $connected_entity_entry->getKey() }}"
-
-                      @if( ( old( $field["name"] ) && in_array($connected_entity_entry->getKey(), old( $field["name"])) ) || (isset($field['value']) && in_array($connected_entity_entry->getKey(), $field['value']->pluck($connected_entity_entry->getKeyName(), $connected_entity_entry->getKeyName())->toArray())))
-                             checked = "checked"
-                      @endif > {!! $connected_entity_entry->{$field['attribute']} !!}
-                  </label>
-                </div>
-            </div>
+        	{{ $field['value'] }}
+        	{{ $connected_entity_entry->getKey() }}
+            
         @endforeach
     </div>
 
