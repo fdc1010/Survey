5fa5b11a75b9c19cf1f93c441bb8597f104006c4
franklin caruana <franklincaruana@gmail.com>
2018-12-10 00:59:49 +0800

    updating

--- app/Http/Requests/SitioRequest.php
+++ app/Http/Requests/SitioRequest.php
@@ -27,7 +27,7 @@ class SitioRequest extends FormRequest
     {
         return [
             // 'name' => 'required|min:5|max:255'
-			'name' => 'required|unique',
+			'name' => 'required|unique:sitios',
         ];
     }
 
