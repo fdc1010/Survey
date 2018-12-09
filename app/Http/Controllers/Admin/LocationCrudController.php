21e70793b6e750eb568817bb32cc9ac733bee74a
franklin caruana <franklincaruana@gmail.com>
2018-12-10 03:20:00 +0800

    updating

--- app/Http/Controllers/Admin/LocationCrudController.php
+++ app/Http/Controllers/Admin/LocationCrudController.php
@@ -34,19 +34,83 @@ class LocationCrudController extends CrudController
 
         // TODO: remove setFromDb() and manually define Fields and Columns
         $this->crud->setFromDb();
-		$this->crud->removeColumns(['area','name','description','municipality_id','barangay_id','sitio_id']);
-		$this->crud->removeFields(['area','name','description','municipality_id','barangay_id','sitio_id']);
-		$this->crud->addColumn([   // Enum
-			'name' => 'area',
-			'label' => 'Area',
-			'type' => 'enum'
-		]);
-		$this->crud->addField([   // Enum
-			'name' => 'area',
-			'label' => 'Area',
-			'type' => 'checklistchkallenum',
-			'enum' => 'area'
-		]);
+		$this->crud->removeColumns(['area_id','name','description','municipality_id','barangay_id','sitio_id']);
+		$this->crud->removeFields(['area_id','name','description','municipality_id','barangay_id','sitio_id']);
+		$this->crud->addColumn([
+            'name' => 'name',
+            'type' => 'text',
+            'label' => 'Name'
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'area_id',
+            'type' => 'select',
+            'label' => 'Area',
+			'entity' => 'area', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\LocationArea"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'municipality_id',
+            'type' => 'select',
+            'label' => 'Municipality',
+			'entity' => 'municipality', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Municipality"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'barangay_id',
+            'type' => 'select',
+            'label' => 'Barangay',
+			'entity' => 'barangay', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Barangay"
+	    ]);
+		$this->crud->addColumn([
+            'name' => 'sitio_id',
+            'type' => 'select',
+            'label' => 'Sitio',
+			'entity' => 'sitio', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Sitio"
+	    ]);
+		$this->crud->addField([
+            'name' => 'name',
+            'type' => 'text',
+            'label' => 'Name'
+	    ]);
+		$this->crud->addField([
+            'name' => 'area_id',
+            'type' => 'checkbox',
+            'label' => 'Area',
+			'entity' => 'area', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\LocationArea"
+	    ]);
+		$this->crud->addField([
+            'name' => 'municipality_id',
+            'type' => 'select2',
+            'label' => 'Municipality',
+			'entity' => 'municipality', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Municipality"
+	    ]);
+		$this->crud->addField([
+            'name' => 'barangay_id',
+            'type' => 'select2',
+            'label' => 'Barangay',
+			'entity' => 'barangay', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Barangay"
+	    ]);
+		$this->crud->addField([
+            'name' => 'sitio_id',
+            'type' => 'select2',
+            'label' => 'Sitio',
+			'entity' => 'sitio', // the relationship name in your Model
+			'attribute' => 'name', // attribute on Article that is shown to admin
+			'model' => "App\Models\Sitio"
+	    ]);
+		
         // add asterisk for fields that are required in LocationRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
