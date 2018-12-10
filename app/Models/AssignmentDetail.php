fba2d6ffb9d378ef22605a7b30ac6d399c3eb55d
franklin caruana <franklincaruana@gmail.com>
2018-12-10 17:05:45 +0800

    updating

--- app/Models/AssignmentDetail.php
+++ app/Models/AssignmentDetail.php
@@ -62,8 +62,7 @@ class AssignmentDetail extends Model
 		
 		$result = "<div class='progress'>".
 					  "<div class='progress-bar' style='width:".$this->getProgress()."%;'>".$this->getProgressPercent()."</div>".
-					"</div>";	
-		echo ($result);
+					"</div>";
 		return $result;
 	}
 	public function getProgress(){
