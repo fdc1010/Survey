77215cca3dc73d42be7e31712a0bd6668af4d8b3
franklin caruana <franklincaruana@gmail.com>
2018-12-10 16:09:52 +0800

    updating

--- app/Http/Controllers/Admin/SurveyorAssignmentCrudController.php
+++ app/Http/Controllers/Admin/SurveyorAssignmentCrudController.php
@@ -56,7 +56,7 @@ class SurveyorAssignmentCrudController extends CrudController
             'name' => 'progress',			
             'label' => 'Progress',
             'type' => 'model_function',
-			'function_name' => 'getProgressBar'
+			'function_name' => 'getProgressPercent'
 	    ]);
 		$this->crud->addColumn([
             'label' => "Survey",
@@ -165,10 +165,10 @@ class SurveyorAssignmentCrudController extends CrudController
 										->get();
 		$result = "<h4>Assigned Areas:</h4><div class='col-lg-6'>";
 		foreach($areas as $area){
-			$result .= "<div class='col-lg-3'>".$area->sitio->name.
-						"</div><div class='col-lg-3'>quota: ".$area->quota.
-						"</div><div class='col-lg-3'>count: ".$area->getSurveyCount().
-						"</div><div class='col-lg-3'>progress: ".$area->getProgressBar().
+			$result .= "<div class='col-lg-2'>".$area->sitio->name.
+						"</div><div class='col-lg-2'>quota: ".$area->quota.
+						"</div><div class='col-lg-2'>count: ".$area->getSurveyCount().
+						"</div><div class='col-lg-6'>progress: ".$area->getProgressBar().
 						"</div>";
 		}
 		$result .= "</div>";
