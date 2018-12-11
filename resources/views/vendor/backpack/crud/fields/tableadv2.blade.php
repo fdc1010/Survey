3a716eae6edb69b03532687e8a2a27ac6daa449f
franklin caruana <franklincaruana@gmail.com>
2018-12-11 18:41:07 +0800

    updating

--- resources/views/vendor/backpack/crud/fields/tableadv2.blade.php
+++ resources/views/vendor/backpack/crud/fields/tableadv2.blade.php
@@ -197,8 +197,8 @@
         {{-- YOUR CSS HERE --}}
         <!-- include select2 css-->
         <link href="{{ asset('vendor/adminlte/bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
-        <!--<link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />-->
-        <link href="{{ asset('css/select2-bootstrap.css') }}" rel="stylesheet" type="text/css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <!--<link href="{{ asset('css/select2-bootstrap.css') }}" rel="stylesheet" type="text/css" />-->
     @endpush
 
     {{-- FIELD JS - will be loaded in the after_scripts section --}}
@@ -207,8 +207,9 @@
         <!-- include select2 js-->
         <script src="{{ asset('vendor/adminlte/bower_components/select2/dist/js/select2.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/angular.min.js') }}"></script>
-        <script type="text/javascript" src="{{ asset('js/jquery-1.12.4.js') }}"></script>
-        <script type="text/javascript" src="{{ asset('js/sortable.min.js') }}"></script>        
+        <!--<script type="text/javascript" src="{{ asset('js/jquery-1.12.4.js') }}"></script>-->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="{{ asset('js/sortable.min.js') }}"></script>        
 		<script>
 
             window.angularApp = window.angularApp || angular.module('backPackTableApp', ['ui.sortable'], function($interpolateProvider){
