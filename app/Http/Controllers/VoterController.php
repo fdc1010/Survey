3a8146c70231548cb4ac781aadf2baabc026d7ff
franklin caruana <franklincaruana@gmail.com>
2019-03-03 13:28:36 +0800

    updating

--- app/Http/Controllers/VoterController.php
+++ app/Http/Controllers/VoterController.php
@@ -50,6 +50,8 @@ class VoterController extends Controller
             $firstname = str_replace($voter->middle_name, '', $voter->first_name);
             if(!empty($firstname)){
                 echo "ID# " . $voter->id . " " . $firstname ." <br>";
+                $updateData = Voter::where('voter_id',$voter->id)
+                                  ->update(['first_name' => $firstname]);
             }else {
                 echo "ID# " . $voter->id . " BLANK first name";
             }
