955bb975f118dc494f6ad0fb713f3e3e9f9ae9e3
franklin caruana <franklincaruana@gmail.com>
2019-03-06 07:48:13 +0800

    updating

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -64,6 +64,7 @@ class SurveyAnswerController extends Controller
                                                     ->whereIn('option_id',[10,11,12,13,14,15,16,17])
                                                     ->whereIn('candidate_id',[3,4,5,6,7,8,9,10,11,23,24,25,26,27,28,36,37])
                                                     ->orderBy('id')
+                                                    ->skip($i)
                                                     ->take(3)
                                                     ->get();
                   if(!empty($tallyothervote) && count($tallyothervote)>0){
