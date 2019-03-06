60b534273e065920481689d4e3dd3df93b929455
franklin caruana <franklincaruana@gmail.com>
2019-03-06 08:52:59 +0800

    updating

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -51,7 +51,7 @@ class SurveyAnswerController extends Controller
                                             ->orderBy('id')
                                             ->take(3)
                                             ->get()->toArray();
-                  dd($surveyansocs[1]);
+                  dd($surveyansocs[1]['id']);
                   $tallyothervotes = TallyOtherVote::where('voter_id',$survey->voter_id)
                                                     ->where('survey_detail_id',1)
                                                     ->whereNull('barangay_id')
