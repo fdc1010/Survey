f7cd318315523d6454ba3ba3dd0c9073d3ff10ba
franklin caruana <franklincaruana@gmail.com>
2019-03-06 08:16:16 +0800

    updating

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -55,6 +55,7 @@ class SurveyAnswerController extends Controller
                                             ->where('survey_detail_id',1)
                                             ->where('voter_id',$survey->voter_id)
                                             ->orderBy('id')
+                                            ->take(3)
                                             ->get();
 
                   $tallyothervote = TallyOtherVote::where('voter_id',$survey->voter_id)
@@ -63,6 +64,7 @@ class SurveyAnswerController extends Controller
                                                     ->whereIn('option_id',[10,11,12,13,14,15,16,17])
                                                     ->whereIn('candidate_id',[3,4,5,6,7,8,9,10,11,23,24,25,26,27,28,36,37])
                                                     ->orderBy('id')
+                                                    ->take(3)
                                                     ->get();
                   var_dump($tallyothervote);
                   if(!empty($tallyothervote) && count($tallyothervote)>0){
