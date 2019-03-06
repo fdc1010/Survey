52d886d7e3aee1f5cd7c17d83f19a24b16ff1734
franklin caruana <franklincaruana@gmail.com>
2019-03-06 10:38:26 +0800

    updating

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -91,7 +91,7 @@ class SurveyAnswerController extends Controller
       //     if($i>2)
       //       $i=0;
                  $surveyansocs = SurveyAnswer::with('option')
-                                            ->whereIn('option_id',[10,11,12,13,14,15,16,17])
+                                            //->whereIn('option_id',[10,11,12,13,14,15,16,17])
                                             ->where('question_id',5)
                                             ->where('survey_detail_id',1)
                                             ->orderBy('id')
@@ -100,7 +100,7 @@ class SurveyAnswerController extends Controller
                       $tallyothervotes = TallyOtherVote::where('voter_id',$survey->voter_id)
                                                         ->where('survey_detail_id',1)
                                                         ->whereNull('barangay_id')
-                                                        ->whereIn('option_id',[10,11,12,13,14,15,16,17])
+                                                        //->whereIn('option_id',[10,11,12,13,14,15,16,17])
                                                         ->where('question_id',5)
                                                         //->whereIn('candidate_id',[27,28,41,42,43])
                                                         //->whereIn('candidate_id',[1,2,16,18,19])
