aef844d8dda3f3e411e8c1b687b78fa2e252945a
franklin caruana <franklincaruana@gmail.com>
2019-03-08 01:28:48 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -2618,14 +2618,14 @@ $(document).ready(function ($) {
 		  x: 'Barangays',
 		  columns: [
 		  	['Barangays',
-			@foreach($tallyp[$barangay->id] as $key => $sortedtallyp)
+			@foreach($tallyp as $key => $sortedtallyp)
 				'{{ $tallybarangay[$key] }}',
 			@endforeach
 			],
 			@foreach($problems as $problem)
 				['{{ $problem->option->option }}',
-				@foreach($tallyp[$barangay->id] as $key => $sortedtallyp)
-					{{ $sortedtallyp[$problem->option_id][$surveydetail->id] }},
+				@foreach($tallyp as $key => $sortedtallyp)
+					{{ $sortedtallyp[$surveydetail->id] }},
 				@endforeach
 				],
 			@endforeach
