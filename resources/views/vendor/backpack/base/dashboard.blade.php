9ca6ff1c6daa7ef2e97dd1b584d6210dec8c1d89
franklin caruana <franklincaruana@gmail.com>
2019-03-08 01:39:36 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1461,9 +1461,9 @@
                                             @endphp
                                         @endforeach
                                         @php
-                                        arsort($tallyp[$barangay->id]);
+                                        arsort($tallyp);
                                         @endphp
-                                        @foreach($tallyp[$barangay->id] as $key => $sortedtallyp)
+                                        @foreach($tallyp as $key => $sortedtallyp)
                                             <tr>
                                                 <td>{{ ++$i . ".) " . $tallybarangay[$key] }}</td>
                                                 @php
@@ -2618,13 +2618,13 @@ $(document).ready(function ($) {
 		  x: 'Barangays',
 		  columns: [
 		  	['Barangays',
-			@foreach($tallyp[$barangay->id] as $key => $sortedtallyp)
+			@foreach($tallyp as $key => $sortedtallyp)
 				'{{ $tallybarangay[$key] }}',
 			@endforeach
 			],
 			@foreach($problems as $problem)
 				['{{ $problem->option->option }}',
-				@foreach($tallyp[$barangay->id] as $key => $sortedtallyp)
+				@foreach($tallyp as $key => $sortedtallyp)
 					{{ $sortedtallyp[$problem->option_id][$surveydetail->id] }},
 				@endforeach
 				],
