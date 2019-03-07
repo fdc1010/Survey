b745084ce93e4d88e3471cdf2a6bfb388157aedc
franklin caruana <franklincaruana@gmail.com>
2019-03-08 02:18:23 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1475,7 +1475,7 @@
                                                     if(empty($tallytotalvpbarangay[$problem->option_id][$surveydetail->id])){
                                                         $tallytotalvpbarangay[$problem->option_id][$surveydetail->id] = $sortedtallyp[$problem->option_id][$surveydetail->id];
                                                     }else{
-                                                        $tallytotalvpbarangay[$surveydetail->id] += $sortedtallyp[$problem->option_id][$surveydetail->id];
+                                                        $tallytotalvpbarangay[$problem->option_id][$surveydetail->id] += $sortedtallyp[$problem->option_id][$surveydetail->id];
                                                     }
                                                     $tallytotalpbarangay += $sortedtallyp[$problem->option_id][$surveydetail->id];
                                                 @endphp
