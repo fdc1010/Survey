89381fcf8b5e811d2a32a68890d5b4af1cd58650
franklin caruana <franklincaruana@gmail.com>
2019-03-08 14:18:32 +0800

    updating

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -535,10 +535,6 @@
                   </div>
                   <div class="col-md-1"><a href="#" id="btn_agedetails"><span class="fa fa-plus" id="spanagedetails"> </span></a></div>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <div class="box box-default">
                 <div class="box-body">
                   <div class="col-md-2">
                     <select name="selgender" id="selgender" style="width: 120px;">
