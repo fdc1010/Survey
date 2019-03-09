ef3fe74ad2d8b561d4695500b68b2d06b3e7b58f
franklin caruana <franklincaruana@gmail.com>
2019-03-10 05:22:00 +0800

    updating

--- app/Http/Controllers/SurveyAnswerController.php
+++ app/Http/Controllers/SurveyAnswerController.php
@@ -724,7 +724,7 @@ class SurveyAnswerController extends Controller
 
                               if(!empty($dupsurans)){
                                 $y++;
-                                echo "<hr>First Entry! ";
+                                echo "<hr>Entry! ";
                                 echo "<br>#".$suranswer->id." ,survey detail id: ".$suranswer->survey_detail_id." ,voter id: ".$suranswer->voter_id." ".$suranswer->voter->full_name." ,question_id: ".$suranswer->question_id." ,option id: ".$suranswer->option_id." ,user id: ".$suranswer->user_id." ".$suranswer->user->name;
                                 echo "<br>Duplicate Entry! ";
                                 echo "<br>#".$dupsurans->id." ,survey detail id: ".$dupsurans->survey_detail_id." ,voter id: ".$dupsurans->voter_id." ".$dupsurans->voter->full_name." ,question_id: ".$dupsurans->question_id." ,option id: ".$dupsurans->option_id." ,user id: ".$dupsurans->user_id." ".$dupsurans->user->name;
