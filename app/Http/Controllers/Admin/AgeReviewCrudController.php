e9aefd68a6e949cabc9af7242ab3f479653164fb
Franklin Caruana <franklincaruana@gmail.com>
2019-03-30 07:51:42 +0800

    fixing

--- app/Http/Controllers/Admin/AgeReviewCrudController.php
+++ app/Http/Controllers/Admin/AgeReviewCrudController.php
@@ -27,7 +27,7 @@ class AgeReviewCrudController extends CrudController
         $this->crud->setEntityNameStrings('age review', 'Age Reviews');
         $this->crud->enableExportButtons();
         $this->crud->denyAccess(['update', 'create', 'delete']);
-        }
+        
         /*
         |--------------------------------------------------------------------------
         | CrudPanel Configuration
