1003dfd15f53b9ae890ff4f6e05d611ad43e8028
Franklin Caruana <franklincaruana@gmail.com>
2019-03-30 07:48:34 +0800

    fixing

--- app/Http/Controllers/Admin/AgeReviewCrudController.php
+++ app/Http/Controllers/Admin/AgeReviewCrudController.php
@@ -49,7 +49,16 @@ class AgeReviewCrudController extends CrudController
 
         // TODO: remove setFromDb() and manually define Fields and Columns
         $this->crud->setFromDb();
-        
+        $this->crud->removeColumns(['voter_id']);
+    		$this->crud->removeFields(['voter_id']);
+        $this->crud->addColumn([
+          'label' => "Voter",
+    			'type' => 'select',
+    			'name' => 'voter_id', // the relationship name in your Model
+    			'entity' => 'voter', // the relationship name in your Model
+    			'attribute' => 'full_name', // attribute on Article that is shown to admin
+    			'model' => "App\Models\Voter" // on create&update, do you need to add/delete pivot table entries?
+  	    ]);
         // add asterisk for fields that are required in AgeReviewRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
