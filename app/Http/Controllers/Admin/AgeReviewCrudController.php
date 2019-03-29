f96a446c9370dd32d1a79a458adbe785dc77ddaa
Franklin Caruana <franklincaruana@gmail.com>
2019-03-30 07:50:22 +0800

    fixing

--- app/Http/Controllers/Admin/AgeReviewCrudController.php
+++ app/Http/Controllers/Admin/AgeReviewCrudController.php
@@ -26,20 +26,7 @@ class AgeReviewCrudController extends CrudController
         $this->crud->setRoute(config('backpack.base.route_prefix') . '/agereview');
         $this->crud->setEntityNameStrings('age review', 'Age Reviews');
         $this->crud->enableExportButtons();
-        if(backpack_user()->hasPermissionTo('Edit')){
-          $this->crud->allowAccess(['update']);
-        }else{
-          $this->crud->denyAccess(['update']);
-        }
-        if(backpack_user()->hasPermissionTo('Add')){
-          $this->crud->allowAccess(['create']);
-        }else{
-          $this->crud->denyAccess(['create']);
-        }
-        if(backpack_user()->hasPermissionTo('Delete')){
-          $this->crud->allowAccess(['delete']);
-        }else{
-          $this->crud->denyAccess(['delete']);
+        $this->crud->denyAccess(['update', 'create', 'delete']);
         }
         /*
         |--------------------------------------------------------------------------
