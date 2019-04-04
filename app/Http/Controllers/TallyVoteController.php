7dcf8b859d396dfc30633d0003420204e1b04271
franklin caruana <franklincaruana@gmail.com>
2019-04-04 16:35:59 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -96,7 +96,7 @@ class TallyVoteController extends Controller
           $delTallyVotetotal += $delTallyVotes->count();
           echo "Current Entry:#".$tallyVote->id." Survey ID:#".$tallyVote->id." | ".$tallyVote->user_id." | ".$tallyVote->voter_id." | ".$tallyVote->question_id."<br>";
           foreach($delTallyVotes as $delTallyVote){
-            echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->id."" | ".$delTallyVote->user_id." | ".$delTallyVote->voter_id." | ".$delTallyVote->question_id."<br>";
+            echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->id." | ".$delTallyVote->user_id." | ".$delTallyVote->voter_id." | ".$delTallyVote->question_id."<br>";
           }
       }
       echo $delTallyVotetotal;
