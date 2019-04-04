46534b98e46041a9dbcb52e9b04ccee116c15895
Franklin Caruana <franklincaruana@gmail.com>
2019-04-05 00:03:22 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -109,7 +109,7 @@ class TallyVoteController extends Controller
                                               //if(in_array($delTallyVote->option_id,[49,50,51,52])){
                                                 echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->survey_detail_id." | ".$delTallyVote->user_id." | ".$delTallyVote->voter_id." | Question & Answer: ".$delTallyVote->question_id." | ".$delTallyVote->option_id."<br>";
                                                 $delTallyVotetotal++;// += $delTallyVotes->count();
-                                                //$delTallyVotes->delete();
+                                                $delTallyVotes->delete();
                                               //}
                                             }
                                             echo "======================================<br>";
