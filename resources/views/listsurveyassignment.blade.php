be48657df39fe99fddb39dcc04abfdcd0aabb1c1
Franklin Caruana <franklincaruana@gmail.com>
2019-04-04 12:18:47 +0800

    updating

--- resources/views/listsurveyassignment.blade.php
+++ resources/views/listsurveyassignment.blade.php
@@ -37,7 +37,7 @@
           @include('crud::inc.filters_navbar')
         @endif
 				@php
-				echo $crud->filters[0]->currentValue;
+				var_dump($crud->filters[0]);
 				$surveyorassignment = $crud->getModel();
 
 				$totalquota = $surveyorassignment->getAllSurveyQuota(); //$surveyorassignment->sum('quota');
