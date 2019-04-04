692620c387469dd191508775a260bb468bdb6e0d
Franklin Caruana <franklincaruana@gmail.com>
2019-04-05 01:16:45 +0800

    updating

--- app/Models/TallyVote.php
+++ app/Models/TallyVote.php
@@ -96,7 +96,7 @@ class TallyVote extends Model
 							$civilstatus = [], $occstatus = [], $voterstatus = []){
 		return $this->where('candidate_id',$candidateid)
 					->where('survey_detail_id',$surveydetailid)
-          ->where('question_id',4)
+          //->where('question_id',4)
           ->has('surveyanswer')
 					->whereHas('voter',function($q)use($agebrackets,$brgy,$genders,
 															$empstatus,$civilstatus,
