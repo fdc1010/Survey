10328ca91f71b93f420fb623dc73ce80c7525a33
Franklin Caruana <franklincaruana@gmail.com>
2019-04-05 00:33:26 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -90,11 +90,11 @@ class TallyVoteController extends Controller
                                //->whereNotIn('id',[7634,7651,7657])
                                ->orderBy('question_id')
                                //->whereIn('option_id',[49,50,51,52])
-                               ->chunk(1400, function ($tallyVotes)use(&$delTallyVotetotal){
+                               ->chunk(400, function ($tallyVotes)use(&$delTallyVotetotal){
 
                                     foreach($tallyVotes as $tallyVote){
                                         $delTallyVotes = TallyVote::where('survey_detail_id',$tallyVote->survey_detail_id)
-                                                                  ->where('id','>',$tallyVote->id)
+                                                                  ->where('id','<>',$tallyVote->id)
                                                                   //->whereNotIn('id',[7635,7636,7652,7653])
                                                                   ->where('question_id',$tallyVote->question_id)
                                                                   //->where('user_id',$tallyVote->user_id)
