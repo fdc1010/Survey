ae65daf644d427e68dab6fcb574ffb994d793103
Franklin Caruana <franklincaruana@gmail.com>
2019-04-04 22:53:39 +0800

    updating

--- app/Models/SurveyorAssignment.php
+++ app/Models/SurveyorAssignment.php
@@ -79,8 +79,7 @@ class SurveyorAssignment extends Model
 		return (($this->count/$this->quota)*100);
 	}
   public function getAllSurveyCount(){
-    $countsurvey = SurveyAnswer::where('survey_detail_id',$this->survey_detail_id)
-                                ->select(['voter_id'])
+    $countsurvey = SurveyAnswer::select(['voter_id'])
                                 ->groupBy('voter_id') // Set by default to Question ID 4. for Mayor... Update this if id changes..
                                 ->get();
 		if($countsurvey)
