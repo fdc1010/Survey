d74983f1fc5ec85810fc7cfb961e9336c0c717fa
Franklin Caruana <franklincaruana@gmail.com>
2019-04-04 23:16:48 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -91,8 +91,8 @@ class TallyVoteController extends Controller
                                     ->where('question_id',$tallyVote->question_id)
                                     ->where('user_id',$tallyVote->user_id)
                                     ->where('voter_id',$tallyVote->voter_id)
-                                    //->whereIn('option_id',[49,50,51,52])
                                     ->whereIn('question_id',[3,4,6,8])
+                                    ->whereIn('option_id',[49,50,51,52])
                                     ->get();
                                     //->delete();
           if(!empty($delTallyVotes) && count($delTallyVotes)>0){
