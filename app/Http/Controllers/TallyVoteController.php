4717e7b5a35e69cda2e8007ea1004236e3793fdb
Franklin Caruana <franklincaruana@gmail.com>
2019-04-05 00:07:40 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -94,7 +94,7 @@ class TallyVoteController extends Controller
 
                                     foreach($tallyVotes as $tallyVote){
                                         $delTallyVotes = TallyVote::where('survey_detail_id',$tallyVote->survey_detail_id)
-                                                                  ->where('id','>',$tallyVote->id)
+                                                                  ->where('id','<>',$tallyVote->id)
                                                                   //->whereNotIn('id',[7635,7636,7652,7653])
                                                                   ->where('question_id',$tallyVote->question_id)
                                                                   ->where('user_id',$tallyVote->user_id)
@@ -109,7 +109,7 @@ class TallyVoteController extends Controller
                                               //if(in_array($delTallyVote->option_id,[49,50,51,52])){
                                                 echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->survey_detail_id." | ".$delTallyVote->user_id." | ".$delTallyVote->voter_id." | Question & Answer: ".$delTallyVote->question_id." | ".$delTallyVote->option_id."<br>";
                                                 $delTallyVotetotal++;// += $delTallyVotes->count();
-                                                TallyVote::find($delTallyVote->id)->delete();
+                                                //TallyVote::find($delTallyVote->id)->delete();
                                               //}
                                             }
                                             echo "======================================<br>";
