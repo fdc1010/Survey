454ae1e35666677f84db8e444c3b0232bdbf38d8
Franklin Caruana <franklincaruana@gmail.com>
2019-04-04 23:28:06 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -85,8 +85,8 @@ class TallyVoteController extends Controller
     public function deleteVoterDuplicateTally(Request $request){
       $delTallyVotetotal = 0;
       $tallyVotes = TallyVote::where('survey_detail_id',2)
-                               ->whereIn('question_id',[3,4,6,8])
-                              // ->whereIn('option_id',[49,50,51,52])
+                               //->whereIn('question_id',[3,4,6,8])
+                               //->whereIn('option_id',[49,50,51,52])
                               ->get();
       foreach($tallyVotes as $tallyVote){
           $delTallyVotes = TallyVote::where('survey_detail_id',$tallyVote->survey_detail_id)
@@ -94,7 +94,7 @@ class TallyVoteController extends Controller
                                     ->where('question_id',$tallyVote->question_id)
                                     ->where('user_id',$tallyVote->user_id)
                                     ->where('voter_id',$tallyVote->voter_id)
-                                    ->whereIn('option_id',[49,50,51,52])
+                                    //->whereIn('option_id',[49,50,51,52])
                                     ->get();
                                     //->delete();
           if(!empty($delTallyVotes) && count($delTallyVotes)>0){
@@ -104,6 +104,7 @@ class TallyVoteController extends Controller
                 //if(in_array($delTallyVote->option_id,[49,50,51,52])){
                   echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->survey_detail_id." | ".$delTallyVote->user_id." | ".$delTallyVote->voter_id." | Question & Answer: ".$delTallyVote->question_id." | ".$delTallyVote->option_id."<br>";
                   $delTallyVotetotal += $delTallyVotes->count();
+                  //$delTallyVotes->delete();
                 //}
               }
               echo "======================================<br>";
