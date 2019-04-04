5122de84b55ef562124a56448e191e9dcdbcd184
Franklin Caruana <franklincaruana@gmail.com>
2019-04-05 01:20:57 +0800

    updating

--- app/Models/TallyVote.php
+++ app/Models/TallyVote.php
@@ -140,7 +140,7 @@ class TallyVote extends Model
 							})
             ->select(['voter_id'])
             ->groupBy('voter_id')
-						->sum('tally');
+						->count('tally');
 	}
   public function tallydetails($candidateid=1,$surveydetailid=1,$agebrackets,$brgyid=0,$civilstatusid=0,$empstatusid=0,$occstatusid=0,$voterstatusid=0,$genderid=0){
 		return $this->where('candidate_id',$candidateid)
