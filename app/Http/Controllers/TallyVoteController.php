07a38b1a92dc2de776e7bf01348f40ab75496be0
franklin caruana <franklincaruana@gmail.com>
2019-04-08 14:02:30 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -139,11 +139,13 @@ class TallyVoteController extends Controller
                                                                   ->get();
                                                                   //->delete();
                                         if(!empty($delTallyVotes) && count($delTallyVotes)>0){
-                                            echo "Current Entry:#".$tallyVote->id." Survey ID:#".$tallyVote->survey_detail_id." | ".$tallyVote->user_id." | ".$tallyVote->voter_id." | Question & Answer: ".$tallyVote->question_id." | ".$tallyVote->option_id."<br>";
+                                            $tallyOption = QuestionOption::find($tallyVote->option_id);
+                                            echo "Current Entry:#".$tallyVote->id." Survey ID:#".$tallyVote->survey_detail_id." | user: ".$tallyVote->user_id." | voter: ".$tallyVote->voter_id." | Question & Answer: ".$tallyVote->question_id." | option: ".$tallyVote->option_id." (".$tallyOption->option.")<br>";
                                             echo "======================================<br>";
                                             foreach($delTallyVotes as $delTallyVote){
+                                              $delTallyOption = QuestionOption::find($delTallyVote->option_id);
                                               //if(in_array($delTallyVote->option_id,[49,50,51,52])){
-                                                echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->survey_detail_id." | ".$delTallyVote->user_id." | ".$delTallyVote->voter_id." | Question & Answer: ".$delTallyVote->question_id." | ".$delTallyVote->option_id."<br>";
+                                                echo "Duplicate Entry:#".$delTallyVote->id." Survey ID:#".$delTallyVote->survey_detail_id." | user: ".$delTallyVote->user_id." | voter: ".$delTallyVote->voter_id." | Question & Answer: ".$delTallyVote->question_id." | option: ".$delTallyVote->option_id." (".$delTallyOption->option.")<br>";
                                                 $delTallyVotetotal++;// += $delTallyVotes->count();
                                                 //SurveyAnswer::find($delTallyVote->id)->delete();
                                               //}
