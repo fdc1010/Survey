18f10c83c4f504d19bdf6b35bce3851f5221bbbb
franklin caruana <franklincaruana@gmail.com>
2019-04-08 19:41:12 +0800

    updating

--- app/Models/TallyVote.php
+++ app/Models/TallyVote.php
@@ -173,7 +173,7 @@ class TallyVote extends Model
 
       $tallyrecs = $tallyraw->get();
       foreach($tallyrecs as $tallyrec){
-        info("#".$tallyrec->id." ".$tallyrec->voter->id." : ".$tallyrec->voter->full_name);
+        info("#".$tallyrec->id." | qid: ".$tallyrec->question_id." | optid: ".$tallyrec->option_id." ".$tallyrec->voter->id." : ".$tallyrec->voter->full_name);
       }
       //$sqlmsg = $tally->toSql();
       //info($sqlmsg);
