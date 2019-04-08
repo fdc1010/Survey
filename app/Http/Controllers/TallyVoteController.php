74985387af0f65422e02a218c2eded3c5990d26b
franklin caruana <franklincaruana@gmail.com>
2019-04-08 14:22:30 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -128,7 +128,7 @@ class TallyVoteController extends Controller
       $tallyVotes = SurveyAnswer::where('survey_detail_id',2)
                                //->where('id','<>',7634)
                                ->whereIn('question_id',[4,3,6,8])
-                               ->whereIn('option_id',[49,50,51,52])
+                               ->where('option_id','>',48)
                                ->where('voter_id','>',231937)
                                ->orderBy('question_id')
                                ->get();//(400, function ($tallyVotes)use(&$delTallyVotetotal){
@@ -138,7 +138,7 @@ class TallyVoteController extends Controller
                                                                   ->where('question_id',$tallyVote->question_id)
                                                                   //->where('user_id',$tallyVote->user_id)
                                                                   ->where('voter_id',$tallyVote->voter_id)
-                                                                  ->whereIn('option_id',[49,50,51,52])
+                                                                  ->where('option_id','>',48)
                                                                   ->where('voter_id','>',231937)
                                                                   ->get();
                                                                   //->delete();
