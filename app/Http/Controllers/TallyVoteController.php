cedb3d19a6099cbd49b7a49bc4affde9a86794c4
Franklin Caruana <franklincaruana@gmail.com>
2019-04-09 21:29:11 +0800

    updating

--- app/Http/Controllers/TallyVoteController.php
+++ app/Http/Controllers/TallyVoteController.php
@@ -235,8 +235,8 @@ class TallyVoteController extends Controller
       $tallyVotes = TallyVote::where('survey_detail_id',2)
                                //->where('option_id','>',48)
                                //->where('question_id','>',3)
-                               ->where('voter_id','>',231937)
-                               ->where('barangay_id',82)
+                               //->where('voter_id','>',231937)
+                               //->where('barangay_id',82)
                                ->orderBy('voter_id')
                                ->get();//chunk(400, function ($tallyVotes)use(&$delTallyVotetotal){
                                     foreach($tallyVotes as $tallyVote){
