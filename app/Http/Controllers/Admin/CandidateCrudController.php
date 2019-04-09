084714969b8aa8e0165bcf4e936b050d37be79b9
Franklin Caruana <franklincaruana@gmail.com>
2019-04-09 13:42:14 +0800

    updating

--- app/Http/Controllers/Admin/CandidateCrudController.php
+++ app/Http/Controllers/Admin/CandidateCrudController.php
@@ -72,7 +72,7 @@ class CandidateCrudController extends CrudController
 			'type' => 'select',
 			'name' => 'voter_id', // the relationship name in your Model
 			'entity' => 'voter', // the relationship name in your Model
-			'attribute' => 'full_name', // attribute on Article that is shown to admin
+			'attribute' => 'id_full_name', // attribute on Article that is shown to admin
 			'model' => "App\Models\Voter" // on create&update, do you need to add/delete pivot table entries?
 	    ]);
 		// $this->crud->addField([
